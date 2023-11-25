72fe7b15d3ed29a5e7a7f5e5f5b5ebd718bf0229
suryasv0729 <suryasv0729@gmail.com>
2023-11-25 22:36:12 +0530

    cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -113,10 +113,6 @@ window.history.pushState(null, '' , `#${model.state.recipe.id}`);
   }
 }
 
-const newFeature = function(){
-  console.log('welcome to the application');
-}
-
 
 const init= function(){
   bookmarksView.addHandlerRender(controlBookmarks)
@@ -126,6 +122,6 @@ const init= function(){
   searchView.addHandlerSearch(controlSearchResults); 
   paginationView.addHandlerClick(controlPagination);
   AddRecipeView.addHandlerUpload(controlUpload)
-newFeature();
+
 }
 init();
